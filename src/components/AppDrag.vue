6537abbfb7585162ce9aa3625165cee2a36f8a72
Lemonka93 <lerayepishko@gmail.com>
2022-11-24 23:18:03 +0300

    first commit

---
+++ src/components/AppDrag.vue
@@ -0,0 +1,22 @@
+<template>
+  <div draggable @dragenter.prevent @dragover.prevent @dragstart.self="onDrag">
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    transferData: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    onDrag(e) {
+      e.dataTransfer.effectAllowed = "move";
+      e.dataTransfer.dropEffect = "move";
+      e.dataTransfer.setData("payload", JSON.stringify(this.transferData));
+    }
+  }
+};
+</script>
